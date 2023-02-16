4cbade4fb2345a2c4307a9016e42233d203abb91
SunMind <sun.mindaugas@gmail.com>
2023-02-16 11:26:44 +0200

    Fresh application

---
+++ src/main/java/task/ignitis/IgnitisApplication.java
@@ -0,0 +1,13 @@
+package task.ignitis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnitisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgnitisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/task/ignitis/IgnitisApplicationTests.java
@@ -0,0 +1,13 @@
+package task.ignitis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnitisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
